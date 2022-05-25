a7e1307463110c3d15c351c623cc6eabf691147b
6204062630084 <s6204062630084@email.kmutnb.ac.th>
2022-05-26 01:55:25 +0700

    oreo

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import './App.css';
 import {
   BrowserRouter as Router,
   Route,
-  Switch,Link
+  Switch
 } from "react-router-dom";
+import { Navbar,Container,Nav ,NavDropdown} from 'react-bootstrap';
 import Menu from './Component/Menu/Menu';
 import Binarystyle from './Component/RootOfEquation/BisectionAndFalsePosition/BisectionAndFalsePosition'
 import Newtonrapson from './Component/RootOfEquation/NR/NR';
@@ -25,8 +26,37 @@ function App() {
   return (
     <div>
       <Router>
-        <Menu/>
-        <Link to='/newton-rapson'>Newtonrapson</Link>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container>
+              <Navbar.Brand href="/">Numer</Navbar.Brand>
+              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+              <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="me-auto">
+                  <NavDropdown title="Root of Equation" id="collasible-nav-dropdown">
+                    <NavDropdown.Item href="/bisection">Bisection Method</NavDropdown.Item>
+                    <NavDropdown.Item href="/false-position">False-Position Method</NavDropdown.Item>
+                    <NavDropdown.Item href="/one-point-iteration">One-point-iteration Method</NavDropdown.Item>
+                    <NavDropdown.Item href="/newton-rapson">Newtonrapson</NavDropdown.Item>
+                    <NavDropdown.Item href="/secant">Secant Method</NavDropdown.Item>
+                  </NavDropdown>
+                  <NavDropdown title="Liner Algebra" id="collasible-nav-dropdown">
+                    <NavDropdown.Item href="/cramerule">Cramer Method</NavDropdown.Item>
+                    <NavDropdown.Item href="/GuessEliminateMethod">Guess Eliminate Method</NavDropdown.Item>
+                    <NavDropdown.Item href="/GaussJordanMethod">Gauss Jordan Eliminate Method</NavDropdown.Item>
+                    <NavDropdown.Item href="/JocobiIterationMethod">Jocobi Iteration Method</NavDropdown.Item>
+                    <NavDropdown.Item href="/GaussSeidelIterationMethod">Gauss Seidel Iteration Method</NavDropdown.Item>
+                    <NavDropdown.Item href="/ConjugateGradientMethod">Conjugate Gradient Method</NavDropdown.Item>
+                  </NavDropdown>
+                  <NavDropdown title="Intepolation and Extrapolation" id="collasible-nav-dropdown">
+                    <NavDropdown.Item href="/Newtondivideddifferences">Newton divided differences</NavDropdown.Item>
+                    <NavDropdown.Item href="/Largrange">Largrange Intepolation differences</NavDropdown.Item>
+                    <NavDropdown.Item href="/spline">Spline</NavDropdown.Item>
+                  </NavDropdown>
+                  <Nav.Link href="/Regression">Regression</Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
         <Switch>
             <Route path="/newton-rapson" exact component={Newtonrapson} />  
         </Switch>
